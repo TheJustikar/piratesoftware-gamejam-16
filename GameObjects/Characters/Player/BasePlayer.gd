3db234f151fc0250e10c9b982d0508bf809ff149
TheJustikar <20945299+TheJustikar@users.noreply.github.com>
2025-01-19 11:59:44 +0100

    added init

--- GameObjects/Characters/Player/BasePlayer.gd
+++ GameObjects/Characters/Player/BasePlayer.gd
@@ -1,1 +1,13 @@
 class_name Player extends Character
+
+func _init(upgrades: Array[Upgrade], health: int, defense: int, damage: int, hitRate: int) -> void:
+	var actualHealth = health
+	var actualDefense = defense
+	var actualDamage = damage
+	var actualHitRate = hitRate
+	for upgrage in upgrades:
+		actualHealth += upgrage.health
+		actualDefense += upgrage.defense
+		actualDamage += upgrage.damage
+		actualHitRate += upgrage.hitRate
+	super(actualHealth, actualDefense, actualDamage, actualHitRate)
