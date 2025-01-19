d6340e658c2420b1899520e135c732d6794be248
TheJustikar <20945299+TheJustikar@users.noreply.github.com>
2025-01-19 11:59:30 +0100

    fix damage taken function

--- GameObjects/Characters/BaseCharacter.gd
+++ GameObjects/Characters/BaseCharacter.gd
@@ -8,6 +8,11 @@ var hitRate: int
 
 func shouldAttack(currentTime: int) -> bool: return (currentTime % hitRate) == 0
 
-func takeDamage(damage: int): health -= int(damage * (defense / 100))
+func takeDamage(damage: int): 
+	var totalDamage = int(damage * (defense / 100))
+	if totalDamage > health:
+		health = 0
+	else:
+		health -= totalDamage
 
 func isAlive() -> bool: return health > 0
