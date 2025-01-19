9119e387de260abb3ff42faab7ce2567abe116cc
TheJustikar <20945299+TheJustikar@users.noreply.github.com>
2025-01-19 18:43:04 +0100

    load 3 random upgrades on start

--- Scenes/Upgrade/upgrade_scene.gd
+++ Scenes/Upgrade/upgrade_scene.gd
@@ -1,5 +1,21 @@
 extends Control
 
 
+@onready
+var upgrades: Array[Upgrade] = []
+
+func _ready() -> void:
+	var pickedUpgrades = []
+	
+	while pickedUpgrades.size() != 3:
+		var upgrade = Upgrade.allUpgrades.pick_random()
+		if pickedUpgrades.has(upgrade) == false:
+			pickedUpgrades.append(upgrade)
+	
+	for i in range(3):
+		var upgrade = pickedUpgrades[i].new()
+		upgrades.append(upgrade)
+
+
 func _on_confirm_button_button_up() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Battle/BattleScene.tscn")
