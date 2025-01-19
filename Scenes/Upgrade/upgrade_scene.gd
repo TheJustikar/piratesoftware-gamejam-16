345afc1a794a62aac620a084940472700a7bb310
TheJustikar <20945299+TheJustikar@users.noreply.github.com>
2025-01-19 18:44:37 +0100

    select upgrade

--- Scenes/Upgrade/upgrade_scene.gd
+++ Scenes/Upgrade/upgrade_scene.gd
@@ -4,6 +4,9 @@ extends Control
 @onready
 var upgrades: Array[Upgrade] = []
 
+var currentSelection: int = -1
+
+
 func _ready() -> void:
 	$CharacterStatsLabel.text = "Character:\n%s" % Global.player().getStatsString()
 	
@@ -22,4 +25,23 @@ func _ready() -> void:
 
 
 func _on_confirm_button_button_up() -> void:
+	Global.upgrades.append(upgrades[currentSelection])
 	get_tree().change_scene_to_file("res://Scenes/Battle/BattleScene.tscn")
+
+
+func _on_select_upgrade_1_button_toggled(toggled_on: bool) -> void:
+	$ConfirmButton.disabled = false
+	if toggled_on:
+		currentSelection = 0
+
+
+func _on_select_upgrade_2_button_toggled(toggled_on: bool) -> void:
+	$ConfirmButton.disabled = false
+	if toggled_on:
+		currentSelection = 1
+
+
+func _on_select_upgrade_3_button_toggled(toggled_on: bool) -> void:
+	$ConfirmButton.disabled = false
+	if toggled_on:
+		currentSelection = 2
