c9171c821188e16565a27b44f9b92038f2efab3a
TheJustikar <20945299+TheJustikar@users.noreply.github.com>
2025-01-19 12:33:59 +0100

    dont save character

--- GameObjects/Global.gd
+++ GameObjects/Global.gd
@@ -1,8 +1,15 @@
 extends Node
 
-var character: Character = null
 var upgrades: Array[Upgrade] = []
+var progress: int = 0
 
 func reset():
-	character = null
 	upgrades = []
+	progress = 0
+	
+func player() -> Player:
+	return Player.new(upgrades, 100, 0, 10, 10)
+	
+func currentEnemy() -> Enemy:
+	#TODO check enemy according to progress
+	return Enemy.new(100, 0, 10, 10)
