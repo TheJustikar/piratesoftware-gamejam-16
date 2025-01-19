f20a6535e4c38dbadc98a8d1770cd3d9ed3ecb8c
TheJustikar <20945299+TheJustikar@users.noreply.github.com>
2025-01-19 18:44:25 +0100

    update labels

--- Scenes/Upgrade/upgrade_scene.gd
+++ Scenes/Upgrade/upgrade_scene.gd
@@ -5,6 +5,8 @@ extends Control
 var upgrades: Array[Upgrade] = []
 
 func _ready() -> void:
+	$CharacterStatsLabel.text = "Character:\n%s" % Global.player().getStatsString()
+	
 	var pickedUpgrades = []
 	
 	while pickedUpgrades.size() != 3:
@@ -15,6 +17,8 @@ func _ready() -> void:
 	for i in range(3):
 		var upgrade = pickedUpgrades[i].new()
 		upgrades.append(upgrade)
+		get_node(NodePath("Upgrades/SelectUpgrade%sButton" % (i + 1))).text = upgrade.name
+		get_node(NodePath("Upgrades/Upgrade%sStatsLabel" % (i + 1))).text = upgrade.getStatsString()
 
 
 func _on_confirm_button_button_up() -> void:
