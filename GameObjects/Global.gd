655d369bae8f16a2765455e06fdd424b608efcee
TheJustikar <20945299+TheJustikar@users.noreply.github.com>
2025-01-19 16:49:42 +0100

    add name to tests

--- GameObjects/Global.gd
+++ GameObjects/Global.gd
@@ -8,8 +8,8 @@ func reset():
 	progress = 0
 	
 func player() -> Player:
-	return Player.new(upgrades, 100, 0, 10, 10)
+	return Player.new(upgrades, "test", 100, 0, 10, 10)
 	
 func currentEnemy() -> Enemy:
 	#TODO check enemy according to progress
-	return Enemy.new(100, 0, 10, 15)
+	return Enemy.new("test", 100, 0, 10, 15)
