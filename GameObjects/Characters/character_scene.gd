8c702c0fd859b266d87fec970d96e1ecfbc1fedd
TheJustikar <20945299+TheJustikar@users.noreply.github.com>
2025-01-21 21:52:46 +0100

    add theme to character sprites

--- GameObjects/Characters/character_scene.gd
+++ GameObjects/Characters/character_scene.gd
@@ -8,7 +8,7 @@ var showStats: Stats = Stats.NONE
 
 func intializeWith(character: Character):
 	self._character = character
-	$NameContainer/NameLabel.text = character.name
+	$Control/NameContainer/NameLabel.text = character.name
 	if character.spritePath.length() > 0:
 		$AnimatedSprite2D.sprite_frames = load(character.spritePath)
 	update()
@@ -16,14 +16,14 @@ func intializeWith(character: Character):
 func update(time: int = 0):
 	match showStats:
 		Stats.NONE:
-			$BattleStatsContainer.visible = false
-			$AllStatsContainer.visible = false
+			$Control/BattleStatsContainer.visible = false
+			$Control/AllStatsContainer.visible = false
 		Stats.BATTLE:
-			$BattleStatsContainer.visible = true
-			$AllStatsContainer.visible = false
-			$BattleStatsContainer/VBoxContainer/TimeToAttackLabel.text = "Attacks in %s" % _character.timeToAttack(time)
-			$BattleStatsContainer/VBoxContainer/HPLabel.text = "HP: %s" % _character.health
+			$Control/BattleStatsContainer.visible = true
+			$Control/AllStatsContainer.visible = false
+			$Control/BattleStatsContainer/VBoxContainer/TimeToAttackLabel.text = "Attacks in %s" % _character.timeToAttack(time)
+			$Control/BattleStatsContainer/VBoxContainer/HPLabel.text = "HP: %s" % _character.health
 		Stats.ALL:
-			$BattleStatsContainer.visible = false
-			$AllStatsContainer.visible = true
-			$AllStatsContainer/AllStatsLabel.text = _character.getStatsString()
+			$Control/BattleStatsContainer.visible = false
+			$Control/AllStatsContainer.visible = true
+			$Control/AllStatsContainer/AllStatsLabel.text = _character.getStatsString()
