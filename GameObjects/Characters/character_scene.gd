6449cf1639170f7cecf49a2eacd02926d20acb64
TheJustikar <20945299+TheJustikar@users.noreply.github.com>
2025-01-21 16:45:25 +0100

    use state for stats

--- GameObjects/Characters/character_scene.gd
+++ GameObjects/Characters/character_scene.gd
@@ -1,17 +1,29 @@
 extends Node2D
 
+enum Stats {NONE, BATTLE, ALL}
+
 var _character: Character
 @export
-var showCombatStats: bool = false
+var showStats: Stats = Stats.NONE
 
 func intializeWith(character: Character):
 	self._character = character
-	$TopCenterContainer/NameLabel.text = character.name
+	$NameContainer/NameLabel.text = character.name
 	if character.spritePath.length() > 0:
 		$AnimatedSprite2D.sprite_frames = load(character.spritePath)
 	update()
 
 func update(time: int = 0):
-	$BottomCenterContainer.visible = showCombatStats
-	$BottomCenterContainer/VBoxContainer/TimeToAttackLabel.text = "Attacks in %s" % _character.timeToAttack(time)
-	$BottomCenterContainer/VBoxContainer/HPLabel.text = "HP: %s" % _character.health
+	match showStats:
+		Stats.NONE:
+			$BattleStatsContainer.visible = false
+			$AllStatsContainer.visible = false
+		Stats.BATTLE:
+			$BattleStatsContainer.visible = true
+			$AllStatsContainer.visible = false
+			$BattleStatsContainer/VBoxContainer/TimeToAttackLabel.text = "Attacks in %s" % _character.timeToAttack(time)
+			$BattleStatsContainer/VBoxContainer/HPLabel.text = "HP: %s" % _character.health
+		Stats.ALL:
+			$BattleStatsContainer.visible = false
+			$AllStatsContainer.visible = true
+			$AllStatsContainer/AllStatsLabel.text = _character.getStatsString()
