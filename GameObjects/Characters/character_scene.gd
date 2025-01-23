e00bc2ef2f3fc3f1e2b75af687db795127131b84
TheJustikar <20945299+TheJustikar@users.noreply.github.com>
2025-01-23 13:48:01 +0100

    cleanup

--- GameObjects/Characters/character_scene.gd
+++ GameObjects/Characters/character_scene.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 enum Stats {NONE, BATTLE, ALL}
 
-var _character: Character
+var _character: Character = EvilEye.new()
 @export
 var showStats: Stats = Stats.NONE
 @export
@@ -13,12 +13,12 @@ func _ready() -> void:
 
 func intializeWith(character: Character):
 	self._character = character
-	$Control/NameContainer/NameLabel.text = character.name
-	if character.spritePath.length() > 0:
-		$AnimatedSprite2D.sprite_frames = load(character.spritePath)
 	update()
 
 func update(time: int = 0):
+	$Control/NameContainer/NameLabel.text = _character.name
+	if _character.spritePath.length() > 0:
+		$AnimatedSprite2D.sprite_frames = load(_character.spritePath)
 	$Control/NameContainer.visible = showName
 	match showStats:
 		Stats.NONE:
@@ -26,12 +26,10 @@ func update(time: int = 0):
 			$Control/AllStatsContainer.visible = false
 		Stats.BATTLE:
 			$Control/AllStatsContainer.visible = false
-			if _character != null:
-				$Control/BattleStatsContainer/VBoxContainer/TimeToAttackLabel.text = "Attacks in %s" % _character.timeToAttack(time)
-				$Control/BattleStatsContainer/VBoxContainer/HPLabel.text = "HP: %s" % _character.health
-				$Control/BattleStatsContainer.visible = true
+			$Control/BattleStatsContainer/VBoxContainer/TimeToAttackLabel.text = "Attacks in %s" % _character.timeToAttack(time)
+			$Control/BattleStatsContainer/VBoxContainer/HPLabel.text = "HP: %s" % _character.health
+			$Control/BattleStatsContainer.visible = true
 		Stats.ALL:
 			$Control/BattleStatsContainer.visible = false
-			if _character != null:
-				$Control/AllStatsContainer/AllStatsLabel.text = _character.getStatsString()
-				$Control/AllStatsContainer.visible = true
+			$Control/AllStatsContainer/AllStatsLabel.text = _character.getStatsString()
+			$Control/AllStatsContainer.visible = true
