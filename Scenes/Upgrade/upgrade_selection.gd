138a865966391f5279122bba1bff31e836d0074f
TheJustikar <20945299+TheJustikar@users.noreply.github.com>
2025-01-24 13:48:29 +0100

    naming

--- Scenes/Upgrade/upgrade_selection.gd
+++ Scenes/Upgrade/upgrade_selection.gd
@@ -12,10 +12,10 @@ func _ready() -> void:
 
 func intializeWith(upgrade: Upgrade):
 	if upgrade.spritePath.length() > 0:
-		$MarginContainer/HBoxContainer/CenterContainer/TextureRect.texture = load(upgrade.spritePath)
+		$MarginContainer/HBoxContainer/IconContainer/TextureRect.texture = load(upgrade.spritePath)
 	
-	$MarginContainer/HBoxContainer/VBoxContainer/NameLabel.text = upgrade.name
-	$MarginContainer/HBoxContainer/VBoxContainer/StatsLabel.text = upgrade.getStatsString()
+	$MarginContainer/HBoxContainer/LabelContainer/NameLabel.text = upgrade.name
+	$MarginContainer/HBoxContainer/LabelContainer/StatsLabel.text = upgrade.getStatsString()
 
 func _on_button_toggled(toggled_on: bool) -> void:
 	if toggled_on:
