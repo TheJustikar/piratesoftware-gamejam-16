b0903f54c70d2528e155762f03d240ed4cef6363
TheJustikar <20945299+TheJustikar@users.noreply.github.com>
2025-01-25 15:21:59 +0100

    add hp color

--- Helpers/colors.gd
+++ Helpers/colors.gd
@@ -3,3 +3,5 @@ extends Node
 static var mainColor: Color = Color("ca3574")
 static var highlightColor: Color = Color("efd7e5")
 static var disabledColor: Color = Color("796374")
+
+static var hpColor: Color = Color("6abe30")
